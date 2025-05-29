ffadda57d62c2bcbd989acc2d11d0b24c9ef026a
umarssalem <umarssalem@gmail.com>
2025-05-29 18:37:59 +0100

    add mock api endpints

--- propers/routes/api.php
+++ propers/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,13 +19,24 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/endpoint', function () {
+Route::get('/endpoint', function (Request $request) {
     $mockData = [
         'id' => rand(1, 100),
         'name' => fake()->name(),
         'email' => fake()->unique()->safeEmail(),
-        'created_at' => now()->toDateTimeString()
+        'created_at' => now()->toDateTimeString(),
     ];
 
     return response()->json($mockData);
-});
+})->middleware('basic.token.auth');
+
+Route::post('/publish/listing', function (Request $request) {
+   
+    $mockData = [
+        'id' => rand(1, 100),
+        'name' => fake()->name(),
+        'email' => fake()->unique()->safeEmail(),
+        'created_at' => now()->toDateTimeString(),
+    ];
+    return response()->json($mockData);
+})->middleware(['basic.token.auth', 'validate.listing.body']);
