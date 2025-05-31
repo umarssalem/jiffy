1b601d26a3c8c1cc14228865ef5fad8a1f826d14
umarssalem <umarssalem@gmail.com>
2025-05-31 01:40:06 +0100

    add special route for dialogflow chatbot

--- propers/routes/api.php
+++ propers/routes/api.php
@@ -22,9 +22,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::middleware(['throttle:api-token'])->group(function () {
-
     Route::get('/availability', [ListingController::class, 'check'])
         ->middleware(['basic.token.auth', 'validate.availability.request']);
+
+    Route::post('/availability', [ListingController::class, 'checkDialogFlowRequest'])
+    ->middleware(['basic.token.auth']);
 
     Route::post('/publish/listing', [ListingController::class, 'publish'])
         ->middleware(['basic.token.auth', 'validate.listing.body']);
